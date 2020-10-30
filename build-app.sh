5f8cbdbefbd41b7090364bd76023d47a6f033cb4
Caitlin Elfring <celfring@gmail.com>
2020-10-30 15:47:35 -0400

    go fmt fixes

--- build-app.sh
+++ build-app.sh
@@ -7,7 +7,7 @@ bash generate-icons.sh
 bash generate-menu-icon.sh
 popd || exit 1
 
-go build github.com/cmmarslender/zoom-status
+go build .
 
 mkdir -p dist/zoom-status.app/Contents/
 mkdir -p dist/zoom-status.app/Contents/MacOS
@@ -16,4 +16,3 @@ mkdir -p dist/zoom-status.app/Contents/Resources
 cp Info.plist dist/zoom-status.app/Contents/
 cp zoom-status dist/zoom-status.app/Contents/MacOS/
 cp icons/icon.icns dist/zoom-status.app/Contents/Resources/
-
--- main.go
+++ main.go
@@ -4,28 +4,28 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/cmmarslender/mack"
-	icon "github.com/cmmarslender/zoom-status/icons"
-	"github.com/getlantern/systray"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/user"
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/andybrewer/mack"
+	icon "github.com/cmmarslender/zoom-status/icons"
+	"github.com/getlantern/systray"
 )
 
 type status struct {
-	StatusText string `json:"status_text"`
+	StatusText  string `json:"status_text"`
 	StatusEmoji string `json:"status_emoji"`
 }
 
 type slackAccount struct {
-	Name string `json:name`
-	Token string `json:token`
-	MeetingStatus *status `json:meetingStatus,omitempty`
-	NoMeetingStatus *status `json:noMeetingStatus,omitempty`
+	Name            string  `json:"name"`
+	Token           string  `json:"token"`
+	MeetingStatus   *status `json:"meetingStatus,omitempty"`
+	NoMeetingStatus *status `json:"noMeetingStatus,omitempty"`
 }
 
 type slackProfile struct {
@@ -44,20 +44,20 @@ var inMeeting = false
 var menuStatus *systray.MenuItem
 
 func main() {
-	systray.Run( onReady, onExit )
+	systray.Run(onReady, onExit)
 }
 
 func onReady() {
-	systray.SetTooltip( "Zoom Status" )
-	systray.SetIcon( icon.Data )
+	systray.SetTooltip("Zoom Status")
+	systray.SetIcon(icon.Data)
 
-	menuStatus = systray.AddMenuItem( "Status: Not In Meeting", "Not In Meeting" )
+	menuStatus = systray.AddMenuItem("Status: Not In Meeting", "Not In Meeting")
 	menuStatus.Disable()
 
 	systray.AddSeparator()
 
 	systray.AddSeparator()
-	mQuit := systray.AddMenuItem( "Quit Zoom Status", "Quit Zoom Status" )
+	mQuit := systray.AddMenuItem("Quit Zoom Status", "Quit Zoom Status")
 	go func() {
 		<-mQuit.ClickedCh
 		systray.Quit()
@@ -70,21 +70,21 @@ func onReady() {
 	for {
 		inMeetingNow := checkForMeeting()
 
-		if true == inMeetingNow {
-			if false == inMeeting {
+		if inMeetingNow {
+			if !inMeeting {
 				setInMeeting()
 			} else {
-				fmt.Println( "Status already set to in meeting" )
+				fmt.Println("Status already set to in meeting")
 			}
 		} else {
-			if true == inMeeting {
+			if inMeeting {
 				setNoMeeting()
 			} else {
-				fmt.Printf( "Status already set to not in meeting \n" )
+				fmt.Printf("Status already set to not in meeting \n")
 			}
 		}
 
-		time.Sleep( 60 * time.Second )
+		time.Sleep(60 * time.Second)
 	}
 }
 
@@ -93,34 +93,32 @@ func onExit() {
 }
 
 func loadConfig() {
-	fmt.Println( "Loading Config..." )
+	fmt.Println("Loading Config...")
 
-	usr, err := user.Current() ; if err != nil {
-		panic( err )
-	}
-
-	jsonFile, err := os.Open( usr.HomeDir + "/slack-status-config.json" ) ; if err != nil {
-		panic( err )
+	jsonFile, err := os.Open(".slack-status-config.json")
+	if err != nil {
+		panic(err)
 	}
 
 	defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll( jsonFile )
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	_ = json.Unmarshal(byteValue, &slackAccounts)
 }
 
 func initMatches() {
-	ignoreMatches = append( ignoreMatches, "Zoom - Pro Account", "Zoom - Free Account" )
-	exactMatches = append( exactMatches, "Zoom" )
-	regexStrings = append( regexStrings, "^Zoom Meeting ID.*" )
+	ignoreMatches = append(ignoreMatches, "Zoom - Pro Account", "Zoom - Free Account")
+	exactMatches = append(exactMatches, "Zoom")
+	regexStrings = append(regexStrings, "^Zoom Meeting ID.*")
 
 	for _, regString := range regexStrings {
-		expr, err := regexp.Compile( regString ) ; if err != nil {
-			panic( err )
+		expr, err := regexp.Compile(regString)
+		if err != nil {
+			panic(err)
 		}
 
-		regexMatches = append( regexMatches, expr )
+		regexMatches = append(regexMatches, expr)
 	}
 }
 
@@ -133,17 +131,18 @@ func initStatuses() {
 }
 
 func checkForMeeting() bool {
-	fmt.Println( "Checking for active meetings..." )
-	result, err := mack.Tell("System Events", "get the title of every window of every process" ) ; if err != nil {
-		panic( err )
+	fmt.Println("Checking for active meetings...")
+	result, err := mack.Tell("System Events", "get the title of every window of every process")
+	if err != nil {
+		panic(err)
 	}
 
-	apps := strings.Split( result, "," )
-	apps = delete_empty( apps )
+	apps := strings.Split(result, ",")
+	apps = delete_empty(apps)
 
 loop1:
 	for _, app := range apps {
-		if strings.Index( app, "Zoom" ) == -1 {
+		if !strings.Contains(app, "Zoom") {
 			continue
 		}
 
@@ -160,7 +159,7 @@ loop1:
 		}
 
 		for _, regexMatch := range regexMatches {
-			if regexMatch.Match( []byte( app ) ) {
+			if regexMatch.Match([]byte(app)) {
 				return true
 			}
 		}
@@ -170,12 +169,12 @@ loop1:
 }
 
 func setInMeeting() {
-	fmt.Println( "Setting status to in meeting" )
+	fmt.Println("Setting status to in meeting")
 	inMeeting = true
 
 	// Set status for all accounts
 	for _, account := range slackAccounts {
-		fmt.Println( "Setting slack status for " + account.Name )
+		fmt.Println("Setting slack status for " + account.Name)
 
 		var profile slackProfile
 		var status status
@@ -188,54 +187,53 @@ func setInMeeting() {
 
 		profile.Profile = status
 
-		setSlackProfile( profile, account.Token )
+		setSlackProfile(profile, account.Token)
 	}
 
-	menuStatus.SetTitle( "Status: In Meeting" )
+	menuStatus.SetTitle("Status: In Meeting")
 }
 
 func setNoMeeting() {
-	fmt.Printf( "Setting status to not in meeting \n" )
+	fmt.Printf("Setting status to not in meeting \n")
 	inMeeting = false
 
 	// Set status for all accounts
 	for _, account := range slackAccounts {
-		fmt.Println( "Setting slack status for " + account.Name )
+		fmt.Println("Setting slack status for " + account.Name)
 
 		var profile slackProfile
-		var status status
+		status := defaultNoMeetingStatus
 
-		if nil != account.NoMeetingStatus {
+		if account.NoMeetingStatus != nil {
 			status = *account.NoMeetingStatus
-		} else {
-			status = defaultNoMeetingStatus
 		}
 
 		profile.Profile = status
 
-		setSlackProfile( profile, account.Token )
+		setSlackProfile(profile, account.Token)
 	}
 
-	menuStatus.SetTitle( "Status: Not In Meeting" )
+	menuStatus.SetTitle("Status: Not In Meeting")
 }
 
-func setSlackProfile( profile slackProfile, token string ) bool {
-	statusBytes, err := json.Marshal( profile ) ; if err != nil {
-		panic( err )
+func setSlackProfile(profile slackProfile, token string) bool {
+	statusBytes, err := json.Marshal(profile)
+	if err != nil {
+		panic(err)
 	}
 
-	req, err := http.NewRequest( "POST", "https://slack.com/api/users.profile.set", bytes.NewBuffer( statusBytes ) )
+	req, err := http.NewRequest("POST", "https://slack.com/api/users.profile.set", bytes.NewBuffer(statusBytes))
 	if err != nil {
-		panic( err )
+		panic(err)
 	}
 
 	// Add proper headers
-	req.Header.Add( "Content-Type", "application/json" )
-	req.Header.Add( "Authorization", "Bearer " + token )
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
 
-	resp, err := http.DefaultClient.Do( req )
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic( err )
+		panic(err)
 	}
 	defer resp.Body.Close()
 
@@ -244,13 +242,13 @@ func setSlackProfile( profile slackProfile, token string ) bool {
 
 /*
 Removes empty strings from a slice of strings
- */
-func delete_empty (s []string) []string {
+*/
+func delete_empty(s []string) []string {
 	var r []string
 	for _, str := range s {
-		str = strings.Trim( str, " " )
+		str = strings.Trim(str, " ")
 		if str != "" {
-			r = append( r, str )
+			r = append(r, str)
 		}
 	}
 	return r
